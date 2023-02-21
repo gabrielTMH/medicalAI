a7094a77fa5cd427b714bb11c2f6cce996fe67b1
Cristian Panaro <cpanaro@lclark.edu>
2023-02-21 14:25:11 -0800

    Separates data by Resolution. Makes every problem have only one resolution.

--- src/resolutionSpreadAlgo.py
+++ src/resolutionSpreadAlgo.py
@@ -2,18 +2,23 @@ import pandas as pd
 
 df = pd.read_excel('TestData.xlsx', na_filter=False)
 
-finalOutput = []
+output = []
 
 for index, row in df.iterrows():
+    otherInfo = row[:"Error Code 3"].to_dict()
+    otherInfo["Resolution 1"] = ""
     rowValue = row
     k = 1
     while True:
         rowLine = "Resolution " + str(k)
-        # print(rowLine)
         if (rowLine in row) and (row[rowLine] != ""):
-            finalOutput.append(index)
+            finalRowInfo = dict(otherInfo)
+            finalRowInfo['Resolution 1'] = row[rowLine]
+            output.append(finalRowInfo)
             k += 1
         else:
             break
 
+finalOutput = pd.DataFrame(output)
+finalOutput.to_csv('TestDataUpdated.csv')
 print(finalOutput)

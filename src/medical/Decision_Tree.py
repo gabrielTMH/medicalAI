607dbc80bf37b72e9c8a0175a194be4fac642f1c
Jessica Erickson <jessicaerickson@lclark.edu>
2023-02-28 14:47:50 -0800

    Copied in good cells from google colab

--- src/medical/Decision_Tree.py
+++ src/medical/Decision_Tree.py
@@ -1,12 +1,42 @@
-import tensorflow_decision_forests as tfdf
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import tensorflow_decision_forests as tfdf
 from tensorflow import keras
-import tfidf
+from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split  # Import train_test_split function
+from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-model = tfdf.keras.RandomForestModel()
-model.fit(tfidf.create_dataset(tfidf.train_df))
-model.compile(metrics=["accuracy"])
-print(model.evaluate(tfidf.create_dataset(tfidf.test_df)))
+pima = pd.read_csv("/content/drive/MyDrive/MEDTEST.csv")
+toto = pd.read_csv("/content/drive/MyDrive/TestDataUpdated.csv")
+y_toto =toto['Sub System']
+x_data=pima.drop('label', axis=1)
+x_data=x_data.fillna(0)
+y_data=pima['label']
+print(x_data)
+
+# preform cell 23 with subsystem data
+X_data = pd.get_dummies(x_data)
+clf = DecisionTreeClassifier()
+y_toto = pd.get_dummies(y_toto)
+Xt_train, Xt_test, t_train, t_test = train_test_split(X_data, y_toto, test_size=.756, random_state=1)
+print(t_test.shape)
+
+clft = clf.fit(Xt_train,t_train)
+t_pred = clft.predict(Xt_test)
+print("Accuracy of toto:",metrics.accuracy_score(t_test, t_pred))
+
+# preform one hot encoding and train and test decision tree on one hot encodings
+X_data=pd.get_dummies(x_data)
+y_data=pd.get_dummies(y_data)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+# Predict the response for test dataset
+y_pred = clf.predict(X_test)
+print(y_test.shape)
+print(y_pred.shape)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
--- src/medical/tfidf.py
+++ src/medical/tfidf.py
@@ -51,6 +51,68 @@ def shuffle_and_split(cleaned_dataFrame):
     return test_df, train_df
 
 
+dataset_df = pd.read_csv("/content/drive/MyDrive/Medical_Error_Test_Data-_1_.csv")
+raw_data = create_dataset(dataFrame)
+cleaned_data = clean_data(raw_data)
+vectorized_data = vectorize_data_set(cleaned_data)
+test_ds, train_ds = shuffle_and_split(vectorized_data)
+print(vectorized_data)
+
+
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+from tensorflow import keras
+
+
+# look into messing around with these numbers
+max_features = 100
+max_len = 10
+
+
+def create_dataFrame_from_csv(name):
+    df = pd.read_csv(name)
+    #todo read in data in a way that can be used by a decision forest and is also mutable
+    dataset = tfdf.keras.pd_dataframe_to_tf_dataset(df)
+    # dataset = tf.data.experimental.CsvDataset(filenames=name)
+    return dataset
+
+
+def create_dataset(dataframe):
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (dataframe["text"].to_numpy(), dataframe["target"].to_numpy())
+    )
+    return tf.reshape(dataset, [1, ])
+
+
+def clean_data(raw_dataFrame):
+    raw_dataFrame['text'] = raw_dataFrame['Interlock 1'] + ' ' + raw_dataFrame['Interlock 2'] + ' ' + raw_dataFrame[
+        'Interlock 3'] + ' ' + \
+                            raw_dataFrame['Error Code 1'] + ' ' + raw_dataFrame['Error Code 2']
+    raw_dataFrame['target'] = raw_dataFrame['Resolution 1']
+    return raw_dataFrame
+
+
+def vectorize_data_set(data_set):
+    vectorizer = tf.keras.layers.TextVectorization(
+        max_tokens=max_features,
+        output_mode='int',
+        output_sequence_length=max_len)
+    vectorizer.adapt(data_set)
+    vectorized_data_set = vectorizer(data_set)
+    print("function ran")
+    return vectorized_data_set
+
+
+def shuffle_and_split(cleaned_dataFrame):
+    df_shuffled = cleaned_dataFrame.sample(frac=1, random_state=42)
+    test_df = df_shuffled.sample(frac=0.1, random_state=42)
+    train_df = df_shuffled.drop(test_df.index)
+    return test_df, train_df
+
+
 dataFrame = create_dataFrame_from_csv("../../data/Medical_Error_Test_Data.csv")
 raw_data = create_dataset(dataFrame)
 cleaned_data = clean_data(raw_data)

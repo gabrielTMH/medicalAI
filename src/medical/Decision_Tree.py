d6fd985d48307e4438e412d4cb9ff425f3554785
Jessica Erickson <jessicaerickson@lclark.edu>
2023-03-02 14:02:58 -0800

    Mid refactoring

--- src/medical/Decision_Tree.py
+++ src/medical/Decision_Tree.py
@@ -8,35 +8,58 @@ from sklearn.model_selection import train_test_split  # Import train_test_split 
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# broad
+classifier = DecisionTreeClassifier()
+'''
+    steps:
+        separate data into resolutions and issues
+        vectorize based on passed function (this has one hot encoding right now)
+        split into test and train f(resolutions, issues, ...)
+        fit classifier
+        
+    
+'''
 
-pima = pd.read_csv("/content/drive/MyDrive/MEDTEST.csv")
-toto = pd.read_csv("/content/drive/MyDrive/TestDataUpdated.csv")
-y_toto =toto['Sub System']
-x_data=pima.drop('label', axis=1)
-x_data=x_data.fillna(0)
-y_data=pima['label']
-print(x_data)
 
-# preform cell 23 with subsystem data
-X_data = pd.get_dummies(x_data)
-clf = DecisionTreeClassifier()
-y_toto = pd.get_dummies(y_toto)
-Xt_train, Xt_test, t_train, t_test = train_test_split(X_data, y_toto, test_size=.756, random_state=1)
-print(t_test.shape)
+# data in
+# I need to get data and redo these two lines
+data = pd.read_csv("/content/drive/MyDrive/MEDTEST.csv") # should just take in one thing of data
+updated_data = pd.read_csv("/content/drive/MyDrive/TestDataUpdated.csv")
 
-clft = clf.fit(Xt_train,t_train)
-t_pred = clft.predict(Xt_test)
-print("Accuracy of toto:",metrics.accuracy_score(t_test, t_pred))
+# labels is the resolutions
+# features is the issues (combination of interlocks and error codes)
+# separate data into resolutions and issues
+updated_data_labels = updated_data['Sub System']  # y label, x data_features
+data_features = data.drop('label', axis=1)  # capitalized post one hot encoding
+data_features = data_features.fillna(0)
+test_data_label = data['label']
 
-# preform one hot encoding and train and test decision tree on one hot encodings
-X_data=pd.get_dummies(x_data)
-y_data=pd.get_dummies(y_data)
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
-clf = DecisionTreeClassifier()
-# Train Decision Tree Classifer
-clf = clf.fit(X_train,y_train)
-# Predict the response for test dataset
-y_pred = clf.predict(X_test)
+
+# Perform cell 23 (from colab) with subsystem data
+one_hot_test_data_features = pd.get_dummies(data_features)  # Was same for both
+
+updated_data_labels = pd.get_dummies(updated_data_labels)
+one_hot_split_train_features, one_hot_split_test_features, one_hot_split_train, one_hot_split_test = train_test_split(one_hot_test_data_features, updated_data_labels, test_size=0.756, random_state=1)
+print(one_hot_split_test.shape)
+
+clft = classifier.fit(one_hot_split_train_features, one_hot_split_train)
+t_pred = clft.predict(one_hot_split_test_features)  # t means has been split into train and test
+print("Accuracy of updated test data:", metrics.accuracy_score(one_hot_split_test, t_pred))
+
+# Perform one hot encoding and train and test decision tree on one hot encodings
+test_data_label = pd.get_dummies(test_data_label)
+X_train, X_test, y_train, y_test = train_test_split(one_hot_test_data_features, test_data_label, test_size=0.3, random_state=42)
+classifier = classifier.fit(X_train, y_train)  # Train Decision Tree Classifier
+y_pred = classifier.predict(X_test)  # Predict the response for test dataset
 print(y_test.shape)
 print(y_pred.shape)
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+def one_hot_vectorizer ():
+    #  TODO function that vectorizes based on ont hot encoding
+
+def vectorize_data ():
+    """
+
+    :return:
+    """

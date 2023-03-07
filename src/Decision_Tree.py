6608d0b5301cec9b215d42beb97c9ee731310954
1dylanangel <1dylanangel@gmail.com>
2023-03-07 14:26:07 -0800

    Cleaned up the main branch, made functions for classes & deleted unused code.

--- src/Decision_Tree.py
+++
@@ -1,6 +0,0 @@
-import tensorflow_decision_forests as tfdf
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-
-model = tfdf.keras.RandomForestModel()
--- src/medical/tf_idf_pynb.py
+++ src/medical/tf_idf_pynb.py
@@ -5,39 +5,41 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 
-v = TfidfVectorizer()
-df = pd.read_csv("../../../../Desktop/reorganized.csv")
 
-has_seen = {}
-count = 0
-row_num = 0
-for label in df.label:
-    if str(label) not in has_seen:
-        df.loc[row_num, 'label_num'] = count
-        has_seen[label] = count
-        count += 1
-    else:
-        df.loc[row_num, 'label_num'] = has_seen[label]
-    row_num += 1
+def tf_idf():
+    v = TfidfVectorizer()
+    df = pd.read_csv("../../../../Desktop/reorganized.csv")
 
-X_train, X_test, y_train, y_test = train_test_split(
-    df.issue,
-    df.label_num,
-    test_size=0.4,
-    random_state=2022,
-    stratify=df.label_num
-)
+    has_seen = {}
+    count = 0
+    row_num = 0
+    for label in df.label:
+        if str(label) not in has_seen:
+            df.loc[row_num, 'label_num'] = count
+            has_seen[label] = count
+            count += 1
+        else:
+            df.loc[row_num, 'label_num'] = has_seen[label]
+        row_num += 1
 
-clf = Pipeline([
-    ('vectorizer_tfidf', TfidfVectorizer()),
-    ('KNN', KNeighborsClassifier())
-])
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.issue,
+        df.label_num,
+        test_size=0.4,
+        random_state=2022,
+        stratify=df.label_num
+    )
 
-# train model
-clf.fit(X_train, y_train)
+    clf = Pipeline([
+        ('vectorizer_tfidf', TfidfVectorizer()),
+        ('KNN', KNeighborsClassifier())
+    ])
 
-# prediction
-y_pred = clf.predict(X_test)
+    # train model
+    clf.fit(X_train, y_train)
 
-# compare
-print(classification_report(y_test, y_pred))
+    # prediction
+    y_pred = clf.predict(X_test)
+
+    # compare
+    print(classification_report(y_test, y_pred))

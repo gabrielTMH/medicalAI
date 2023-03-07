b0d29a76d0fd834072abd327281a3368bc166f7b
Cristian Panaro <cpanaro@lclark.edu>
2023-03-07 13:21:47 -0800

    added tfidf_vectorizer to decision_tree.py

--- src/medical/Decision_Tree.py
+++ src/medical/Decision_Tree.py
@@ -2,13 +2,17 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def Decision_Tree(input_data):
     issues, resolutions = prep_data(input_data)
     vectorized_issues, vectorized_resolutions = vectorize_data(one_hot_vectorizer, issues, resolutions)
     issues_train, issues_test, resolutions_train, resolutions_test = \
-        train_test_split(vectorized_issues, vectorized_resolutions, test_size=0.756, random_state=1)
+        train_test_split(vectorized_issues, vectorized_resolutions, test_size=0.3, random_state=1)
     predict_response(issues_train, issues_test, resolutions_train, resolutions_test)
 
 
@@ -20,11 +24,31 @@ def vectorize_data(vectorizer, issues, resolutions):
     return vectorizer(issues, resolutions)
 
 
-def predict_response(issues_train, issues_test, resolutions_train, resolutions_test):
-    classifier = DecisionTreeClassifier()
+def predict_response(issues_train, issues_test, resolutions_train, resolutions_test, classifier, print_results):
+    classifier = classifier()
     fitted_classifier = classifier.fit(issues_train, resolutions_train)
     prediction = fitted_classifier.predict(issues_test)
+    print_results(resolutions_test, prediction)
+
+
+def one_hot_classifier():
+    return DecisionTreeClassifier()
+
+
+def tfidf_classifier():
+    classifier = Pipeline([
+        ('vectorizer_tfidf', TfidfVectorizer()),
+        ('KNN', KNeighborsClassifier())
+    ])
+    return classifier
+
+
+def one_hot_print_results(resolutions_test, prediction):
     print("Accuracy: ", metrics.accuracy_score(resolutions_test, prediction))
+
+
+def tfidf_print_results(resolutions_test, prediction):
+    print(classification_report(resolutions_test, prediction))
 
 
 def one_hot_vectorizer(issues, resolutions):
@@ -37,9 +61,9 @@ def tfidf_vectorizer(data, issues, resolutions):
     row_num = 0
     for resolution in resolutions:
         if str(resolution) not in has_seen:
-            data.loc[row_num, 'label_num'] = count
+            data.loc[row_num, 'resolution_num'] = count
             has_seen[resolution] = count
             count += 1
         else:
-            data.loc[row_num, 'label_num'] = has_seen[label]
+            data.loc[row_num, 'resolution_num'] = has_seen[resolution]
         row_num += 1

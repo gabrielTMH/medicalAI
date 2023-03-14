fed171e057d2049cf9c3545f85f10c3bbc656509
Cristian Panaro <cpanaro@lclark.edu>
2023-03-14 13:31:45 -0700

    Edited "other codes" columns in app.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+
+# Include the name of all folder or files in your project folder that are nessesary for the project excluding your main flask file.
+# If there are multiple files, you can add them into a folder and then specify the folder name.
+
+includefiles = [ 'templates', 'static', 'app.py',]
+includes = [ 'jinja2' , 'jinja2.ext']
+excludes = ['Tkinter']
+
+setup(
+ name='Medical AI 1.0',
+ version = '0.1',
+ description = 'Sample Flask App',
+ options = {'build_exe':   {'excludes':excludes,'include_files':includefiles, 'includes':includes}},
+ executables = [Executable('app.py')]
+)
